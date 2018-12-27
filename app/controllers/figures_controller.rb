32c8c1f582239e1efb15f0a4ed8f001be9a0771e
Mac Dowell Prosper <macdowell@noukod.com>
2018-12-27 04:47:09 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,26 @@
 class FiguresController < ApplicationController
-  # add controller methods
+
+  get '/figures' do
+    erb :"figures/index"
+  end
+
+  get '/figures/new' do
+    erb :"figures/new"
+  end
+
+  get '/figures/:id' do
+    binding.pry
+    @figure = Figure.find params[:id]
+
+    erb :"figures/show"
+  end
+
+  post '/figures' do
+    # binding.pry
+    figure = Figure.create params[:figure] #new_figure with or without titles and landmarks
+    figure.landmarks << Landmark.create(params[:landmark]) if !params[:landmark].empty? #add new landmark
+    figure.titles << Title.create(params[:title]) if !params[:title].empty? #add new title
+
+    redirect to "/figures/#{figure.id}"
+  end
 end
